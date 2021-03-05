b6ba8b4af76dc3b9239a927d8e296ebed9354a9f
Gustavo Souza <gustavo.souza@postclick.com>
2021-03-05 09:02:05 -0300

    Adding allure and Mochawesome report

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,5 +1,6 @@
 import './commands'
 import '@shelex/cypress-allure-plugin';
+import 'cypress-mochawesome-reporter/register';
 
 
 require('@shelex/cypress-allure-plugin');
@@ -9,5 +10,10 @@ export const credentials = {
     email: "demostration_test@test.com",
     password: "user_testpsw",
 }
+
+Cypress.Screenshot.defaults({
+    screenshotOnRunFailure: false
+  })
+  
 
 
